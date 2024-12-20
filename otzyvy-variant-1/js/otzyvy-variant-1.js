0a5179bc201bb99d0a43efef66367da754eca576
Aliaksei Karzhou <alexius90e@gmail.com>
2024-12-21 02:24:12 +0300

    feat: added otzyvy-variant-1

---
+++ otzyvy-variant-1/js/otzyvy-variant-1.js
@@ -0,0 +1,44 @@
+function countVidyContentMaxHeight() {
+  const oddCards = document.querySelectorAll('.otzyvy-variant-1__card:nth-child(odd)');
+  const evenCards = document.querySelectorAll('.otzyvy-variant-1__card:nth-child(even)');
+
+  const oddCardsHeight = [...oddCards]
+    .map((card) => card.offsetHeight)
+    .reduce((acc, value) => acc + value, 0);
+
+  const evenCardsHeight = [...evenCards]
+    .map((card) => card.offsetHeight)
+    .reduce((acc, value) => acc + value, 0);
+
+  const maxColumnHeight = oddCardsHeight > evenCardsHeight ? oddCardsHeight : evenCardsHeight;
+  const maxColumnElemsCount = oddCardsHeight > evenCardsHeight ? oddCards.length : evenCards.length;
+
+  const container = document.querySelector('.otzyvy-variant-1__content');
+
+  if (container) {
+    const gap = Number.parseInt(window.getComputedStyle(container).columnGap);
+    container.style.maxHeight = `${maxColumnHeight + maxColumnElemsCount * gap}px`;
+  }
+}
+
+window.addEventListener('resize', () => countVidyContentMaxHeight());
+
+countVidyContentMaxHeight();
+
+const otzyvyModal = document.querySelector('.otzyvy-variant-1__modal');
+const otzyvyAddButton = document.querySelector('.otzyvy-variant-1__new-button');
+
+if (otzyvyAddButton && otzyvyModal) {
+  otzyvyAddButton.addEventListener('click', () => {
+    otzyvyModal.classList.add('active');
+  });
+
+  otzyvyModal.addEventListener('click', (event) => {
+    const isLayout = event.target === event.currentTarget;
+    const isClose = event.target.classList.contains('otzyvy-variant-1__modal-close');
+
+    if (isClose || isLayout) {
+      event.currentTarget.classList.remove('active');
+    }
+  });
+}
