7c5b9541459192107a8aed106acdd0262fc4d2d6
Aliaksei Karzhou <alexius90e@gmail.com>
2025-01-09 18:46:02 +0300

    feat: added kviz-v-1 v1.0.1

--- kviz-v-1/js/kviz-v-1/kviz-v-1.js
+++ kviz-v-1/js/kviz-v-1/kviz-v-1.js
@@ -2,14 +2,8 @@ const quizSwiper = new Swiper('.kviz-v-1__quiz .swiper', {
   slidesPerView: 1,
   spaceBetween: 32,
   allowTouchMove: false,
-  breakpoints: {
-    320: {
-      autoHeight: true,
-    },
-    576: {
-      autoHeight: false,
-    },
-  },
+  autoHeight: true,
+
   navigation: {
     nextEl: '.kviz-v-1__quiz-card-controls-next',
     prevEl: '.kviz-v-1__quiz-card-controls-prev',
@@ -19,4 +13,14 @@ const quizSwiper = new Swiper('.kviz-v-1__quiz .swiper', {
 quizSwiper.on('reachEnd', () => {
   const quizBgElem = document.querySelector('.kviz-v-1__quiz-bg');
   if (quizBgElem) quizBgElem.classList.add('reachend');
+});
+
+const quizRadioInputs = document.querySelectorAll(
+  'input[type="radio"].kviz-v-1__quiz-card-inputs-checkbox-input'
+);
+
+quizRadioInputs.forEach((radioInput) => {
+  radioInput.addEventListener('input', () => {
+    quizSwiper.slideNext();
+  });
 });
